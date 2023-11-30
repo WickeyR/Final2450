2316fa20161c0bdd524a62b17c244a5aa9782e63
WickeyR <149520847+WickeyR@users.noreply.github.com>
2023-11-30 10:56:55 -0800

    Updated Images for Article Thumbnails
    Signed-off-by: WickeyR <149520847+WickeyR@users.noreply.github.com>

--- src/ArticleSection.java
+++ src/ArticleSection.java
@@ -66,12 +66,12 @@ public class ArticleSection extends GridPane {
                                   "consolidated subsidiaries for the third quarter and first\n" +
                                 " nine months of 2023 and 2022 are summarized below");
                 articleOneContainer = articleOne.setContainerBehavior();
-                articleTwo = new ArticleImageContainer(new Image("ArticleThumbnails/Q3.png"), "Third Quarter News\nRelease", "Nov 2, 2023", "src/PdfPaths/News Reports/nov0223.pdf",
+                articleTwo = new ArticleImageContainer(new Image("ArticleThumbnails/AnnualReportImgae copy.png"), "Third Quarter News\nRelease", "Nov 2, 2023", "src/PdfPaths/News Reports/nov0223.pdf",
                         "Omaha, NE (BRK.A; BRK.B) – Berkshire Hathaway Inc.’s \n" +
                                 "third quarter earnings release and its quarterly report on \n" +
                                 "Form 10-Q will be posted on the Internet on Saturday, November 4, 2023");
                 articleTwoContainer = articleTwo.setContainerBehavior();
-                articleThree = new ArticleImageContainer(new Image("ArticleThumbnails/news.png"), "General News Release", "Aug 5, 2023", "src/PdfPaths/News Reports/aug0523.pdf",
+                articleThree = new ArticleImageContainer(new Image("ArticleThumbnails/News2Thumbnail copy.png"), "General News Release", "Aug 5, 2023", "src/PdfPaths/News Reports/aug0523.pdf",
                         "Berkshire’s operating results for the second quarter and \n" +
                                 "first six months of 2023 and 2022 are summarized in the\n" +
                                 "following paragraphs");
@@ -88,21 +88,21 @@ public class ArticleSection extends GridPane {
                 articleSectionPane.add(articleFourContainer, 1, 1);
             }
             case 2 -> {
-                articleOne = new ArticleImageContainer(new Image("ArticleThumbnails/news.png"), "Berkshire Hathaway\nDiversity", "2021", "src/PdfPaths/Sustainability/berkshire2021eeo1.pdf",
+                articleOne = new ArticleImageContainer(new Image("ArticleThumbnails/DiversityThumnail copy.png"), "Berkshire Hathaway\nDiversity", "2021", "src/PdfPaths/Sustainability/berkshire2021eeo1.pdf",
                         "1. Based on data filed for all Berkshire Hathaway Inc.\n" +
                                 " subsidiaries in the 2021 Employer Information Report (EEO-1) \n" +
                                 "with the U.S. Equal Employment Opportunity Commission");
                 articleOneContainer = articleOne.setContainerBehavior();
-                articleTwo = new ArticleImageContainer(new Image("ArticleThumbnails/news.png"), "Leadership Council", "2021", "src/PdfPaths/Sustainability/brksustainabilityleadershipcouncilmay2021.pdf",
+                articleTwo = new ArticleImageContainer(new Image("ArticleThumbnails/leadershipCouncil copy.png"), "Leadership Council", "2021", "src/PdfPaths/Sustainability/brksustainabilityleadershipcouncilmay2021.pdf",
                         "The Berkshire Hathaway Sustainability Leadership Council \n" +
                                 "(SLC) is an autonomous group of representatives from Berkshire\n"+
                                 " Hathaway companies committed to championing the");
                 articleTwoContainer = articleTwo.setContainerBehavior();
-                articleThree = new ArticleImageContainer(new Image("ArticleThumbnails/news.png"), "PACIFICORP\nInformation", "2023", "src/PdfPaths/Sustainability/facts_pacificorp.pdf",
+                articleThree = new ArticleImageContainer(new Image("ArticleThumbnails/Pacificorp copy.png"), "PACIFICORP\nInformation", "2023", "src/PdfPaths/Sustainability/facts_pacificorp.pdf",
                         "PacifiCorp is the largest grid operator in the western U.S., \n" +
                                 "serving 2 million customers in six states.");
                 articleThreeContainer = articleThree.setContainerBehavior();
-                articleFour = new ArticleImageContainer(new Image("ArticleThumbnails/news.png"), "Mid American\nEnergy Information", "2023", "src/PdfPaths/Sustainability/facts_midamerican_energy_company.pdf",
+                articleFour = new ArticleImageContainer(new Image("ArticleThumbnails/MidAmericanTHumbnail copy.png"), "Mid American\nEnergy Information", "2023", "src/PdfPaths/Sustainability/facts_midamerican_energy_company.pdf",
                         "MidAmerican Energy Company, based in Des Moines, Iowa, meets \n" +
                                 "customers’ energy needs by providing safe, reliable, affordable\n" +
                                 " and clean energy to customers in Iowa, Illinois, South Dakota and Nebraska.");
@@ -127,13 +127,13 @@ public class ArticleSection extends GridPane {
                 articleSectionPane.add(articleFourContainer, 1, 1);
             }
             case 4 -> {
-                articleOne = new ArticleImageContainer(new Image("ArticleThumbnails/news.png"), "Comparative Rights", "June 20, 2010", "src/PdfPaths/StockHolderArticles/comparativeRights.pdf", " Comparative Rights and Relative prices of \nBerkshire Class A and Class B Stock");
+                articleOne = new ArticleImageContainer(new Image("ArticleThumbnails/comparativeRights copy.png"), "Comparative Rights", "June 20, 2010", "src/PdfPaths/StockHolderArticles/comparativeRights.pdf", " Comparative Rights and Relative prices of \nBerkshire Class A and Class B Stock");
                 articleOneContainer = articleOne.setContainerBehavior();
-                articleTwo = new ArticleImageContainer(new Image("ArticleThumbnails/news.png"), "Energy Presentation", "Nov 2022", "src/PdfPaths/StockHolderArticles/BHE2022InvestPresent.pdf", "Presentation shown at EEI annual confernce");
+                articleTwo = new ArticleImageContainer(new Image("ArticleThumbnails/EnergyPresentationThumbnail copy.png"), "Energy Presentation", "Nov 2022", "src/PdfPaths/StockHolderArticles/BHE2022InvestPresent.pdf", "Presentation shown at EEI annual confernce");
                 articleTwoContainer = articleTwo.setContainerBehavior();
-                articleThree = new ArticleImageContainer(new Image("ArticleThumbnails/news.png"), "Investment Information", "Feb 17, 2022", "src/PdfPaths/StockHolderArticles/FactsAboutInvesting.pdf", "Important information for investors of Berkshire Hathaway");
+                articleThree = new ArticleImageContainer(new Image("ArticleThumbnails/investmentInfo copy.png"), "Investment Information", "Feb 17, 2022", "src/PdfPaths/StockHolderArticles/FactsAboutInvesting.pdf", "Important information for investors of Berkshire Hathaway");
                 articleThreeContainer = articleThree.setContainerBehavior();
-                articleFour = new ArticleImageContainer(new Image("ArticleThumbnails/news.png"), "Stock Holder\n Info", "2022", "src/PdfPaths/StockHolderArticles/commonStock.pdf", "Stock Holder policies and guidelines related to Berkshire Hathaway");
+                articleFour = new ArticleImageContainer(new Image("ArticleThumbnails/StockHolderInfo copy.png"), "Stock Holder\n Info", "2022", "src/PdfPaths/StockHolderArticles/commonStock.pdf", "Stock Holder policies and guidelines related to Berkshire Hathaway");
                 articleFourContainer = articleFour.setContainerBehavior();
                 articleSectionPane.add(articleOneContainer, 0, 0);
                 articleSectionPane.add(articleTwoContainer, 1, 0);
